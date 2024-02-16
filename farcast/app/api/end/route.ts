8b3490319cc4bd0ba3859abb27dc3a1c13797fc4
lectra5iX <39646345+lectra5iX@users.noreply.github.com>
2024-02-16 19:37:22 +0700

    Update route.ts

--- farcast/app/api/end/route.ts
+++ farcast/app/api/end/route.ts
@@ -1,3 +1,4 @@
+// pages/api/end.ts or pages/api/end/index.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
