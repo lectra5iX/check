290ad52feda1882a32aa7e25631df20a4e3a00f6
lectra5iX <39646345+lectra5iX@users.noreply.github.com>
2024-02-17 11:27:14 +0700

    Update page.tsx

--- farcast/app/page.tsx
+++ farcast/app/page.tsx
@@ -8,7 +8,7 @@ const frameMetadata = getFrameMetadata({
       label: "Check Your Eligibility"
     }
   ],
-  image: "https://cdn.discordapp.com/attachments/1050082795629842462/1208033742929662013/Elig1.png?ex=65e1d018&is=65cf5b18&hm=af14db7049acfe5b52b5424bc135bff256eed5e09f965307c1a5b395dce1259c&",
+  image: "https://i.imgur.com/CJC38Tw.png",
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 })
 
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Eligibility Checker',
     description: 'Check your eligibility for the $Frame Earn program',
-    images: ["https://cdn.discordapp.com/attachments/1050082795629842462/1208033742929662013/Elig1.png?ex=65e1d018&is=65cf5b18&hm=af14db7049acfe5b52b5424bc135bff256eed5e09f965307c1a5b395dce1259c&"]
+    images: ["https://i.imgur.com/CJC38Tw.png"]
   },
   other: {
     ...frameMetadata,
