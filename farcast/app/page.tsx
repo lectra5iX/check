ac091cd35c73042afaea84c83cc4818ca4993701
lectra5iX <39646345+lectra5iX@users.noreply.github.com>
2024-02-17 12:10:11 +0700

    Update page.tsx

--- farcast/app/page.tsx
+++ farcast/app/page.tsx
@@ -5,20 +5,20 @@ import type { Metadata } from 'next';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: "Check Your Eligibility"
+      label: "Hi Spacer! Next, please!",
     }
   ],
-  image: "https://i.imgur.com/CJC38Tw.png",
+  image: "https://i.imgur.com/5XXBjb1.png",
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 })
 
 export const metadata: Metadata = {
   title: 'Eligibility Checker',
-  description: 'Check your eligibility for the $Frame Earn program',
+  description: 'Check your eligibility for the $SPACER Earn program',
   openGraph: {
     title: 'Eligibility Checker',
-    description: 'Check your eligibility for the $Frame Earn program',
-    images: ["https://i.imgur.com/CJC38Tw.png"]
+    description: 'Check your eligibility for the $SPACER Earn program',
+    images: ["https://i.imgur.com/5XXBjb1.png"]
   },
   other: {
     ...frameMetadata,
